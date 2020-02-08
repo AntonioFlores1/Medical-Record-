68bba87068e3015507293f8600062c4b9e496b2a
antonio <antonio@antonios-MacBook-Pro.local>
2020-02-08 17:08:27 -0500

    ui profile

--- Medical Records/View Controllers/ProfileViewController.swift
+++ Medical Records/View Controllers/ProfileViewController.swift
@@ -16,14 +16,35 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var profilePicture: UIImageView!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        profilePicture.layer.cornerRadius = profilePicture.frame.size.width/2
+        profileView.layer.borderColor = UIColor.red.cgColor
+        profilePicture.layer.masksToBounds = true
+        profilePicture.layer.borderWidth = 0
+        
+        profileView.layer.borderColor = UIColor.black.cgColor
+        profileView.layer.cornerRadius = 22
         
     }
     
 
     
 
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell
+    }
+    
+    
 }
